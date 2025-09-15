b521d6527f2f03e8e36215152c44efcd67ec90ad
Lynn Lu <lulynn@microsoft.com>
2025-09-14 21:55:16 -0700

    Delete duplicated file

--- src/PredictorCache/CacheKeyGenerator.cs
+++ src/PredictorCache/CacheKeyGenerator.cs
@@ -37,50 +37,7 @@ public class CacheKeyGenerator
         return cacheKey;
     }
 
-    /// <summary>
-    /// Generates multiple cache keys for prefix matching (1 to maxPrefixLength character prefixes)
-    /// This allows "g" to match cached results for "git branch", "git status", etc.
-    /// </summary>
-    public List<string> GenerateAllPrefixKeys(SuggestionRequest request)
-    {
-        var keys = new List<string>();
-        var normalizedInput = NormalizeUserInput(request.UserInput);
-        
-        if (string.IsNullOrEmpty(normalizedInput))
-            return keys;
 
-        // Generate prefix keys from 1 character up to maxPrefixLength
-        var maxLength = Math.Min(maxPrefixLength, normalizedInput.Length);
-        
-        for (int i = 1; i <= maxLength; i++)
-        {
-            keys.Add(normalizedInput.Substring(0, i));
-        }
-
-        return keys;
-    }
-
-    /// <summary>
-    /// Finds matching prefix cache keys for a given input (longest to shortest)
-    /// </summary>
-    public List<string> FindMatchingPrefixKeys(SuggestionRequest request)
-    {
-        var keys = new List<string>();
-        var normalizedInput = NormalizeUserInput(request.UserInput);
-        
-        if (string.IsNullOrEmpty(normalizedInput))
-            return keys;
-
-        // Search from longest possible prefix down to single character
-        var maxLength = Math.Min(maxPrefixLength, normalizedInput.Length);
-        
-        for (int i = maxLength; i >= 1; i--)
-        {
-            keys.Add(normalizedInput.Substring(0, i));
-        }
-
-        return keys;
-    }
 
     /// <summary>
     /// Generates multiple cache keys for prefix matching (1-3 character prefixes)
